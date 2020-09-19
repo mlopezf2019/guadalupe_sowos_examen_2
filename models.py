54831c5224d0089bce7fae956a7fb2afbea3ccb7
Lupita Lopez <mglopez@narrowcastdigital.com>
2020-09-19 11:52:05 -0500

    add created at and updated at

--- models.py
+++ models.py
@@ -3,12 +3,18 @@ from django.db import models
 
 class User(models.Model):
   uuid = models.UUIDField(primary_key = False, default = uuid.uuid4, editable = False)
-  
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+
 class Person(models.Model):
   user_id = models.ForeignKey(User)
   name = models.CharField(max_length=200)
   last_name = models.CharField(max_length=200)
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
 
 class Customer(models.Model):
   person_id = models.ForeignKey(Person)
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
 
